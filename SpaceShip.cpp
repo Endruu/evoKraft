39d619ecb1027148433b9b7b3d880c090000fac1
endruu <bibok.andor@gmail.com>
2019-03-31 00:38:50 +0100

    Fix after merge

--- SpaceShip.cpp
+++ SpaceShip.cpp
@@ -11,139 +11,134 @@ SpaceShip::SpaceShip(int id_, int mapHeight, int mapWidth) :
 
 void SpaceShip::refreshState(const GameState& state)
 {
-    gameState = state;
+	gameState = state;
 
-    auto me = std::find_if(begin(gameState.players), end(gameState.players), [this](const GameObject& obj) { return id == obj.id; });
+	auto me = std::find_if(begin(gameState.players), end(gameState.players), [this](const GameObject& obj) { return id == obj.id; });
 
-    x = me->x;
-    y = me->y;
-    vx = me->vx;
-    vy = me->vy;
-    radius = me->radius;
-    rotation = me->rotation;
-    acceleration = me->acceleration;
-    cooldown = me->cooldown;
+	x = me->x;
+	y = me->y;
+	vx = me->vx;
+	vy = me->vy;
+	radius = me->radius;
+	rotation = me->rotation;
+	acceleration = me->acceleration;
+	cooldown = me->cooldown;
 
-  if (m_lastAbsoluteTargetRotation == rotation) m_rotating = false;
-  if (acceleration == 0.0f) m_accelerating = false;
+	if (m_lastAbsoluteTargetRotation == rotation) m_rotating = false;
+	if (acceleration == 0.0f) m_accelerating = false;
 
-  gameState.players.erase(me);
+	gameState.players.erase(me);
 }
 
 GameCommand SpaceShip::rotateTowards(const GameObject& obj) const
 {
-    std::cout << "m_lastAbsoluteTargetRotation: " << m_lastAbsoluteTargetRotation << std::endl;
-    std::cout << "Self rot: " << rotation << std::endl;
-
-    if (abs(m_lastAbsoluteTargetRotation - absoluteRotation(*this, obj)) < 0.0000001f)
-    {
-        std::cout << "NOTHING" << std::endl;
-        return GameCommand::nothing();
-    }
-
-    m_lastAbsoluteTargetRotation = absoluteRotation(*this, obj);
-
-    return (relativeRotation(*this, obj) > 0) ? GameCommand::rotateLeft(m_lastAbsoluteTargetRotation) : GameCommand::rotateRight(m_lastAbsoluteTargetRotation);
+	return rotateTowards(obj.x, obj.y);
 }
 
 GameCommand SpaceShip::rotateTowards(const float x, const float y) const
 {
-  std::cout << "m_lastAbsoluteTargetRotation: " << m_lastAbsoluteTargetRotation << std::endl;
-  std::cout << "Self rot: " << rotation << std::endl;
+	std::cout << "m_lastAbsoluteTargetRotation: " << m_lastAbsoluteTargetRotation << std::endl;
+	std::cout << "Self rot: " << rotation << std::endl;
 
+	if (abs(m_lastAbsoluteTargetRotation - absoluteRotation(*this, x, y)) < 0.0000001f)
+	{
+		std::cout << "NOTHING" << std::endl;
+		return GameCommand::nothing();
+	}
 
-  if (abs(m_lastAbsoluteTargetRotation - absoluteRotation(*this, x, y)) < 0.0001)
-  {
-    std::cout << "NOTHING" << std::endl;
-    return GameCommand(GameCommand::NOTHING);
-  }
+	m_lastAbsoluteTargetRotation = absoluteRotation(*this, x, y);
 
-  m_lastAbsoluteTargetRotation = absoluteRotation(*this, x, y);
-
-  return GameCommand(
-    (relativeRotation(*this, x, y) > 0) ? GameCommand::ROTATERIGHT : GameCommand::ROTATELEFT,
-    m_lastAbsoluteTargetRotation);
+	return (relativeRotation(*this, x, y) > 0) ? GameCommand::rotateLeft(m_lastAbsoluteTargetRotation) : GameCommand::rotateRight(m_lastAbsoluteTargetRotation);
 }
 
 GameCommand SpaceShip::shoot() const
 {
-    return GameCommand::fire();
+	return GameCommand::fire();
 }
 
 GameCommand SpaceShip::moveTowards(const float x, const float y)
 {
-  //get the coordinates of exactly half the path we want to go, start slowing down when we reach it
-  if ((this->x - avoidDangerX) < 5)
-  {
-    m_accelerating = false;
-    m_moving = false;
-    return GameCommand(GameCommand::STOPACCELERATION);
-  }
-  else if ((startX - ((startX + avoidDangerX) / 2)) < 0)
-  {
-    if (!m_accelerating)
-    {
-      m_accelerating = true;
-      return GameCommand(m_reversed ? GameCommand::DECELERATE : GameCommand::ACCELERATE);
-    }
-  }
-  else
-  {
-    if (!m_accelerating)
-    {
-      m_accelerating = true;
-      return GameCommand(m_reversed ? GameCommand::ACCELERATE : GameCommand::DECELERATE);
-    }
-  }
-    return GameCommand::nothing();
+	//get the coordinates of exactly half the path we want to go, start slowing down when we reach it
+	if ((this->x - avoidDangerX) < 5)
+	{
+		m_accelerating = false;
+		m_moving = false;
+		return GameCommand::stopAcceleration();
+	}
+	else if ((startX - ((startX + avoidDangerX) / 2)) < 0)
+	{
+		if (!m_accelerating)
+		{
+			m_accelerating = true;
+			return m_reversed ? GameCommand::decelerate() : GameCommand::accelerate();
+		}
+	}
+	else
+	{
+		if (!m_accelerating)
+		{
+			m_accelerating = true;
+			return m_reversed ? GameCommand::accelerate() : GameCommand::decelerate();
+		}
+	}
+	return GameCommand::nothing();
 }
 
 GameCommand SpaceShip::move(float x, float y)
 {
-  //determine if we want to accelerate/decelerate, and the way of turning
-  m_moving = true;
-  m_reversed = reverseRotation(*this, x, y);
+	//determine if we want to accelerate/decelerate, and the way of turning
+	m_moving = true;
+	m_reversed = reverseRotation(*this, x, y);
 
-  if (abs(relativeRotation(*this, x, y)) < 0.0001)
-  {
-    if (!startSet)
-    {
-      startX = this->x;
-      startY = this->y;
-      startSet = true;
-    }
+	if (abs(relativeRotation(*this, x, y)) < 0.0001)
+	{
+		if (!startSet)
+		{
+			startX = this->x;
+			startY = this->y;
+			startSet = true;
+		}
 
-    return moveTowards(startX, startY);
-  }
-  else
-  {
-    m_rotating = true;
+		return moveTowards(startX, startY);
+	}
+	else
+	{
+		m_rotating = true;
 
-    return rotateTowards(x, y);
-  }
+		return rotateTowards(x, y);
+	}
+}
+
+bool SpaceShip::isCoveredByObstacles(const GameObject& target) const
+{
+	const auto line = Line(x, y, target.x, target.y);
+	return std::find_if(gameState.obstacles.begin(), gameState.obstacles.end(), [this, &line](const GameObject& obs)
+	{
+		return distanceFromLine(line, obs.x, obs.y) < obs.radius;
+	}) != gameState.obstacles.end();
 }
 
 GameCommand SpaceShip::calculateNextStep()
 {
-    if (gameState.turrets.size() == 0) return GameCommand::nothing();
+	if (gameState.turrets.size() == 0) return GameCommand::nothing();
 
-    GameObject target = gameState.turrets[0];
-    for (const auto& turret : gameState.turrets)
-    {
-        if (abs(relativeRotation(*this, turret)) < abs(relativeRotation(*this, target)))
-        {
-            target = turret;
-        }
-    }
+	GameObject target = gameState.turrets[0];
+	for (const auto& turret : gameState.turrets)
+	{
+		if (abs(relativeRotation(*this, turret)) < abs(relativeRotation(*this, target)))
+		{
+			target = turret;
+		}
+	}
 
-  return move(100, 50);
+	//return move(100, 50);
 
-  /*if (abs(relativeRotation(*this, target)) < 0.1)
-  {
-    return shoot();
-  }
-  else
-  {
-    return rotateTowards(target);
-  }*/
+	if (abs(relativeRotation(*this, target)) < 0.1)
+	{
+	  return shoot();
+	}
+	else
+	{
+	  return rotateTowards(target);
+	}
 }
--- SpaceShip.h
+++ SpaceShip.h
@@ -9,46 +9,46 @@
 class SpaceShip : public GameObject
 {
 public:
-    SpaceShip(int id, int mapHeight, int mapWidth);
+	SpaceShip(int id, int mapHeight, int mapWidth);
 
-    void refreshState(const GameState& state);
+	void refreshState(const GameState& state);
 
-    GameCommand rotateTowards(const GameObject& obj) const;
+	GameCommand rotateTowards(const GameObject& obj) const;
 
-    GameCommand rotateTowards(const float x, const float y) const;
+	GameCommand rotateTowards(float x, float y) const;
 
-    GameCommand shoot() const;
+	GameCommand shoot() const;
 
-    GameCommand moveTowards(const float x, const float y);
+	GameCommand moveTowards(float x, float y);
 
-    GameCommand move(float x, float y);
+	GameCommand move(float x, float y);
 
 	bool isCoveredByObstacles(const GameObject& target) const;
 
-    bool isAccelerating() const { return m_accelerating; }
-    bool isRotating() const { return m_rotating; }
-    bool isMoving() const { return m_moving; }
-    bool isReversed() const { return m_reversed; }
+	bool isAccelerating() const { return m_accelerating; }
+	bool isRotating() const { return m_rotating; }
+	bool isMoving() const { return m_moving; }
+	bool isReversed() const { return m_reversed; }
 
-    GameCommand calculateNextStep();
+	GameCommand calculateNextStep();
 
 private:
 
-    GameState gameState;    
+	GameState gameState;
 
-    float startX;
-    float startY;
-    bool startSet{ false };
+	float startX;
+	float startY;
+	bool startSet{ false };
 
-    bool m_accelerating{ false };
-    bool m_rotating{ false };
-    bool m_moving{ false };
-    bool m_reversed{ false };
+	bool m_accelerating{ false };
+	bool m_rotating{ false };
+	bool m_moving{ false };
+	bool m_reversed{ false };
 
 	struct { float high, low, left, right; } const limits;
 
-    mutable float m_lastAbsoluteTargetRotation = 0.0f;
+	mutable float m_lastAbsoluteTargetRotation = 0.0f;
 
-    float avoidDangerX;
-    float avoidDangerY;
+	float avoidDangerX;
+	float avoidDangerY;
 };
